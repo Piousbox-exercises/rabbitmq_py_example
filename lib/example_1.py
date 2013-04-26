cfc50fe33b4ca30dcf2fc860298fe3aded20dca4
piousbox <piousbox@piousbox-laptop.(none)>
2013-04-26 14:21:03 -0700

    little

--- lib/example_1.py
+++ lib/example_1.py
@@ -2,7 +2,15 @@
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-  'localhost'))
+                                     'piousbox-laptop'))
 
 channel = connection.channel()
 channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+
+print " [x] Sent 'Hello World!'"
+
+connection.close()
